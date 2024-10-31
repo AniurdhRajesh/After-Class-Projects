c34a43e7890cf7a39d13a9b01479b5638a7abd98
aniurdhrajesh <anirudhrajesh04@gmail.com>
2024-10-31 20:11:40 +0530

    UPDATE

---
+++ Birthdays.py
@@ -0,0 +1,8 @@
+Anirudh="02/06/2012"
+Malavika="15/06/2012"
+Advaith="01/12/2012"
+Aradhana="21/12/2012"
+print("The Birthdate of Anirudh is ",Anirudh)
+print("The Birthdate of Advaith is ",Advaith)
+print("The Birthdate of Aradhana is ",Aradhana)
+print("The Birthdate of Malavika is ",Malavika)
